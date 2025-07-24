d4c89a1380a83185b7e02850dd74410c7da592a2
crossur <cruz24955@gmail.com>
2025-07-24 16:05:04 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: './',
     clean: true,
   },
   devServer: {
